0b44959c378d9e977aecb93bcb96073d5d85e396
Allain Lalonde <allain.lalonde@gmail.com>
2015-10-08 17:40:45 -0400

    Treating arguments that don't end with '.txt' as libraries

--- test/cli.js
+++ test/cli.js
@@ -1,10 +1,8 @@
 var test = require('blue-tape');
 
-var Ottomaton = require('..');
-
 test('cli works', function (t) {
   process.chdir(__dirname);
-  require('child_process').exec('../bin/otto --lib ./libraries/a.js --lib ./libraries/b ./cli-test.txt', function (err, out) {
+  require('child_process').exec('../bin/otto ./libraries/a.js ./libraries/b ./cli-test.txt', function (err, out) {
     t.error(err);
     t.equal(out, 'A\nB\n');
     t.end();
@@ -13,7 +11,7 @@ test('cli works', function (t) {
 
 test('piped cli works', function (t) {
   process.chdir(__dirname);
-  require('child_process').exec('cat ./cli-test.txt | ../bin/otto --lib ./libraries/a.js --lib ./libraries/b', function (err, out) {
+  require('child_process').exec('cat ./cli-test.txt | ../bin/otto ./libraries/a.js ./libraries/b', function (err, out) {
     t.error(err);
     t.equal(out, 'A\nB\n');
     t.end();
@@ -24,16 +22,16 @@ test('piped cli works', function (t) {
 test('cli missing lib failures work', function (t) {
   process.chdir(__dirname);
 
-  require('child_process').exec('../bin/otto --lib ./libraries/missing.js ./cli-test.txt', function (err, out, stderr) {
+  require('child_process').exec('../bin/otto ./libraries/missing.js ./cli-test.txt', function (err, out, stderr) {
     t.ok(err);
-    t.equal(stderr, 'ERROR: unable to register library "./libraries/missing.js"\n');
+    t.ok(stderr.match(/ERROR: unable to register library ".*\/libraries\/missing.js"\n$/));
     t.end();
   });
 });
 
 test('cli line errors work', function (t) {
   process.chdir(__dirname);
-  require('child_process').exec('../bin/otto --lib ./libraries/a.js ./cli-test.txt', function (err, out, stderr) {
+  require('child_process').exec('../bin/otto ./libraries/a.js ./cli-test.txt', function (err, out, stderr) {
     t.ok(err);
     t.equal(stderr, 'ERROR: Line Errors:\nUnrecognized Line: #2: b\n');
     t.equal(out, '');
